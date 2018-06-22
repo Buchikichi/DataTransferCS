46f92aa510406c8e3bc85ee3854ce948f9aa12c8
Hidetaka Sasai <sasai@ds-solution.co.jp>
2018-06-22 11:21:30 +0900

    Modify connection string.

--- DataTransfer/MainForm.cs
+++ DataTransfer/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using DataTransfer.Properties;
 using Npgsql;
 using System;
+using System.Data;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -11,18 +12,36 @@ namespace DataTransfer
         #region Events
         private void TestSourceButton_Click(object sender, EventArgs e)
         {
-            var host = Settings.Default.SourceHostName;
-            var port = (int)Settings.Default.SourcePort;
-            var catalog = Settings.Default.SourceSchema;
-            var user = Settings.Default.SourceUser;
-            var password = Settings.Default.SourcePassword;
-            var connString = $"Server={host};Port={port};Database={catalog};User Id={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = Settings.Default.SourceHostName,
+                Port = (int)Settings.Default.SourcePort,
+                Database = Settings.Default.SourceSchema,
+                Username = Settings.Default.SourceUser,
+                Password = Settings.Default.SourcePassword,
+            };
+            if (SourceTlsCheckBox.Checked)
+            {
+                builder.SslMode = SslMode.Require;
+                builder.TrustServerCertificate = true;
+            }
+            var query = "SELECT * FROM pg_tables WHERE tableowner = CURRENT_USER";
 
-            using (var con = new NpgsqlConnection(connString))
+            using (var con = new NpgsqlConnection(builder.ToString()))
             {
                 con.Open();
+                Debug.Print("connected.");
+
+                var cmd = new NpgsqlCommand(query, con);
+                var dataReader = cmd.ExecuteReader();
+
+                foreach (IDataRecord row in dataReader)
+                {
+                    var name = row[1];
+
+                    Debug.Print("name:" + name);
+                }
             }
-            Debug.Print("connected.");
         }
         #endregion
 
